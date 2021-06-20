8d310d109a7ce4e2eaad1b0c3120e3617eace77f
TheInvestmentClub <83925775+TheInvestmentClub@users.noreply.github.com>
2021-06-19 20:58:15 -0400

    Update script.php

--- script.php
+++ script.php
@@ -1,11 +1,8 @@
 <?php
 
 $fileName = "../renames-2021.csv";
-if (($handle = fopen($fileName, 'r')) !== FALSE) { // Check the resource is valid
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) { // Check opening the file is OK!
-        print_r($data); // Array
-    }
-    fclose($handle);
-}
+$file = file_get_contents($file);
+$lines = explode("\n", $file);
+print_r($lines);
 
 ?>
