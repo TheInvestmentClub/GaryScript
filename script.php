4b91d0c079c7e5a535652bcae1ceb766ff437d14
TheInvestmentClub <83925775+TheInvestmentClub@users.noreply.github.com>
2021-06-19 20:49:59 -0400

    Update script.php

--- script.php
+++ script.php
@@ -1,5 +1,12 @@
 <?php
 
-echo "Testing 2 After pull\n";
+$fileName = "../renames-2021.csv";
+$file = file_get_contents($fileName);
+$lines = explode("\n", $file);
+foreach($lines as $line) {
+  [$oldFileName, $newFileName] = explode(",", $line);
+  // Test 
+  print "{$oldFileName} - {$newFileName}/n";
+}
 
 ?>
